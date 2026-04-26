4c14c14ba96fa8179344403636624edd820dab4d
Rafael-Petry <petryrafael29@gmail.com>
2026-04-26 13:37:44 -0300

    Feature: Changes main.cpp to properly call all methods

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,48 @@
 #include "celestialBody/planet/planet.h"
 #include "celestialBody/sun/sun.h"
 
+#include "spaceship/spaceship.h"
+
 int main()
 {
+    Spaceship spaceship;
+
     Asteroid asteroid;
     Sun& sun = Sun::getInstance();
     Planet planet;
 
+    std::cout << "-----Run Spaceship-----" << std::endl;
+    spaceship.translate();
+    spaceship.rotate();
+    spaceship.shoot();
+    std::cout << "Model path: " << Spaceship::getModel() << std::endl;
+    std::cout << "-----------------------" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "-----Run Celestial Body-----" << std::endl;
+    std::cout << "Celestial body model path: " << CelestialBody::getModel() << std::endl;
+    std::cout << "----------------------------" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "------Run Asteroid------" << std::endl;
     asteroid.translate();
     asteroid.rotate();
+    std::cout << "Asteroid color: " << CelestialBody::getColor() << std::endl;
+    std::cout << "------------------------" << std::endl;
+    std::cout << std::endl;
 
+    std::cout << "--------Run Sun--------" << std::endl;
     sun.translate();
     sun.rotate();
+    std::cout << "Sun color: " << Sun::color << std::endl;
+    std::cout << "-----------------------" << std::endl;
+    std::cout << std::endl;
 
+    std::cout << "-------Run Planet------" << std::endl;
     planet.translate();
     planet.rotate();
-
-    std::cout << "Celestial body model path: " << CelestialBody::getModel() << std::endl;
-    std::cout << "Celestial body default color: " << CelestialBody::getColor() << std::endl;
+    std::cout << "Planet color: " << Planet::color << std::endl;
+    std::cout << "-----------------------" << std::endl;
 
     return 0;
 }
